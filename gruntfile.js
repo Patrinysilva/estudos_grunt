ba53843c106e0630af808311ca3274a8d384173c
Patrinysilva <patrinysilva17@gmail.com>
2025-02-10 20:59:59 -0300

    continuação da aula, minificando o html

--- gruntfile.js
+++ gruntfile.js
@@ -22,11 +22,15 @@ module.exports = function(grunt){
             less:{
                 files:['src/styles/**/*.less'],
                 tasks:['less:development']
+            },
+            html:{
+                files: ['src/index.html'],
+                tasks: ['replace:dev']
             }
         },
-        replace{
+        replace: {
             dev: {
-                option: {
+                options: {
                     patterns: [
                         {
                             match: 'ENDERECO_DO_CSS',
@@ -42,8 +46,38 @@ module.exports = function(grunt){
                         dest: 'dev/'
                     }
                 ]
+            },
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.min.css'
+                        }
+                    ]
+                },
+                files:[
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
             }
-        }
+        },
+        htmlmin: {
+            dist: {
+                options:{
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files:{
+                    'prebuild/index.html': 'src/index.html'
+                }
+            }
+        },
+        clean: ['prebuild']
     })
 
 
@@ -51,7 +85,9 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-replace');
-
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['less:production']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean']);
 }
